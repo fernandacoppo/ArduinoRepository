ba1c286a29fa5b1c7c3566f140213cf28360bc00
fernandacoppo <fer_coppo@hotmail.com>
2017-12-18 18:21:58 -0200

    Implementação do código de carregamento da bateria

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ArduinoRepository
 Repositório de códigos em arduino implementados
+
+- Links:
+
+https://docs.google.com/spreadsheets/d/1ByqoqI_zRphjHvcGYYYoLGASK3Y6YVSYh4yP-R0PBH8/edit#gid=0 //Calculadora Divisor de Tensão
---
+++ SmartShelter/ControleCarregamento.ino
@@ -0,0 +1,75 @@
+const int SinalReleBase = 12;
+const int SinalReleApm = 11;
+const int SensorBase = A0;
+const int SensorBateria = A1;
+#define DELTAANALOGV 0.1913580247;
+#define MAXBATERIA 736.4007782;
+#define MAXBASE 796.1089494;
+#define AMOSTRAS 12
+
+void Setup() {
+  pinMode(SinalReleBase,OUTPUT);
+  pinMode(SinalReleApm,OUTPUT);
+  pinMode(SensorBase,INPUT);
+  pinMode(SensorBateria,INPUT);
+}
+
+float LePortaAnalogica(uint8_t portaAnalogica) {
+  float total = 0;  
+  for (int i = 0; i < AMOSTRAS; i++) {
+    total += analogRead(portaAnalogica);
+    delay(5);
+  }
+  return total/(int)AMOSTRAS;
+}
+
+void AcionarRele(uint8_t portaDigital){
+  digitalWrite(portaDigital,HIGH);
+  delay(5);
+}
+
+void AcionarRelesPreCarregamento(){
+  AcionarRele(SinalReleApm);
+  AcionarRele(SinalReleBase);  
+}
+
+void AcionarRelesPosCarregamento(){
+  AcionarRele(SinalReleBase); 
+  AcionarRele(SinalReleApm); 
+}
+
+bool VerificaLeituraAnalogica(uint8_t portaAnalogica, float constanteAnalogica){
+  float leituraAnalogica = lePortaAnalogica(portaAnalogica);
+  if(leituraAnalogica >= (constanteAnalogica - (float)DELTAANALOGV) || leituraAnalogica <= (constanteAnalogica + (float)DELTAANALOGV))
+	return true;
+  return false;
+}
+
+bool VerificaTensaoDaBase(){
+  if (VerificaLeituraAnalogica(SinalReleBase,(float)MAXBASE)))
+    return true;
+  return false;
+}
+
+bool AcompanharCarregamento(){
+  if (VerificaLeituraAnalogica(SinalReleApm,(float)MAXBATERIA))
+  {
+	AcionarRelesPosCarregamento();
+	return true;
+  }
+  return false;
+}
+
+void CarregarBateria(){
+  bool carregou = false;
+  AcionarRelesPreCarregamento();
+  while(carregou = false)
+  {
+	carregou = AcompanharCarregamento()
+  }
+}
+
+void Loop() {
+  if(VerificaTensaoDaBase())
+	  CarregarBateria();
+}
