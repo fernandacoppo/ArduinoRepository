53b9ccef0b5791c2135c02d6efcf65be3649f0c1
Azoia <eduardoazoia@hotmail.com>
2017-12-22 19:09:25 -0200

    Código de teste das funções

--- SmartShelter/ControleCarregamentoTeste/ControleCarregamentoTeste.ino
+++ SmartShelter/ControleCarregamentoTeste/ControleCarregamentoTeste.ino
@@ -2,9 +2,10 @@ const int SinalReleBase = 12;
 const int SinalReleApm = 11;
 const int SensorBase = A0;
 const int SensorBateria = A1;
+#define aRef 5
 #define DELTAANALOGV 0.1913580247
 #define MAXBATERIA 736.4007782
-#define MAXBASE 796.1089494
+#define MAXBASE 746.3521401
 #define AMOSTRAS 12
 
 void setup() {
@@ -21,7 +22,7 @@ float LePortaAnalogica(uint8_t portaAnalogica) {
     total += analogRead(portaAnalogica);
     delay(5);
   }
-  return total/(int)AMOSTRAS;
+  return total/(float)AMOSTRAS;
 }
 
 void AcionarRele(uint8_t portaDigital){
@@ -46,10 +47,10 @@ void AcionarRelesPosCarregamento(){
 
 bool VerificaLeituraAnalogica(uint8_t portaAnalogica, float constanteAnalogica){
   float leituraAnalogica = LePortaAnalogica(portaAnalogica);
-  Serial.print("\n\rConstante analógica é: ");
+  Serial.print("\n\rConstante analÃ³gica Ã©: ");
   Serial.print(leituraAnalogica);
-  Serial.print("\n\rA tensão é: ");
-  Serial.print(leituraAnalogica/1023);
+  Serial.print("\n\rA tensÃ£o Ã©: ");
+  Serial.print((leituraAnalogica/1023)*(float)aRef);
   Serial.println ("V");
   if(leituraAnalogica >= (constanteAnalogica - (float)DELTAANALOGV) || leituraAnalogica <= (constanteAnalogica + (float)DELTAANALOGV))
 	return true;
@@ -69,26 +70,61 @@ bool AcompanharCarregamento(){
 	  AcionarRelesPosCarregamento();
 	  return true;
   }
-  Serial.print("Bateria não está carregada!");
+  Serial.print("Bateria nÃ£o estÃ¡ carregada!");
   return false;
 }
 
 void CarregarBateria(){
   bool carregou = false;
   AcionarRelesPreCarregamento();
-  while(carregou = false)
+  while(carregou == false)
   {
-	carregou = AcompanharCarregamento();
+	  carregou = AcompanharCarregamento();
+  }
+}
+
+void ImprimeNaTela(){
+	Serial.print("\n\rBem vindo ao menu de teste!");
+	Serial.print("\n\rSegue abaixo um menu de opçôes de teste:");
+	Serial.print("\n\r(1) - Testar leitura no sensor de tensão da base:");
+	Serial.print("\n\r(2) - Testar leitura no sensor de tensão da bateria:");
+	Serial.print("\n\r(3) - Testar lógica do sensor da base:");
+	Serial.print("\n\r(4) - Testar lógica do sensor da bateria:");
+	Serial.print("\n\r(5) - Sair!");
+}
+
+void MenuTeste(){
+  bool sair, haTensao = false;
+  int caso;	
+  while(sair != true){
+    if (Serial.available() > 0){
+		caso = Serial.read();
+		
+		switch (caso){
+		case 1:
+			haTensao = VerificaTensaoDaBase();
+		break;
+		case 2:
+			haTensao = VerificaLeituraAnalogica(SinalReleApm,(float)MAXBATERIA);
+		break;
+		case 3:
+			haTensao = VerificaTensaoDaBase();
+			Serial.print("\n\rA lógica é:" + haTensao);
+		break;
+		case 4:
+			haTensao = VerificaLeituraAnalogica(SinalReleApm,(float)MAXBATERIA);
+			Serial.print("\n\rA lógica é:" + haTensao);
+		break;
+		case 5:
+			sair = true;
+		break;
+		}
+	}
   }
 }
 
 void loop() {
-  if(VerificaTensaoDaBase())
-  {
-	  Serial.print("Há tensão na base!");
-	  CarregarBateria();
-	  delay(300000);
-  }
-  Serial.print("Não há tensão na base!");
-  delay(1000);
+	ImprimeNaTela();
+  MenuTeste();
 }
+
